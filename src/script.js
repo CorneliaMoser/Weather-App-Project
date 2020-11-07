5dc7b0de6e4c9fb8cb7a7b3f762fe59ab9af227a
CorneliaMoser <corneliamoser1991@gmail.com>
2020-11-07 18:46:03 +0100

    added forecast javascript

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
 function formatDate(timestamp){
     let date = new Date(timestamp);
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp){
+    let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10){
         hours = `0${hours}`;
@@ -8,9 +16,7 @@ function formatDate(timestamp){
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
 }
 
 function displayTemperature(response){
@@ -38,10 +44,33 @@ function displayTemperature(response){
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function dispalyForecast(response){
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+
+    for (let index = 0; index < 6; index++) {
+        forecast = response.data.list[index];
+        forecastElement.innerHTML +=  `<div class="col-2">
+                <h3>
+                    ${formatHours(forecast.dt * 1000)}
+                </h3>
+                <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png">
+                <div class="weather-forecast-temperature">
+                    <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+                    
+                </div>
+            </div>`;
+    }
+}
+
 function search(city) {
 let apiKey = "aed57f2386376dd257ae589f54ccdbfe"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(dispalyForecast);
 
 }
 
@@ -63,6 +92,8 @@ function displayFahrenheitTemperature(event) {
 
 function displayCelsiusTemperature(event) {
     event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
